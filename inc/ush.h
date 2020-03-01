0eab602070f3b5cf3861204cb77b7cef543d88bf
Anatolii Kostanda <akostanda@e2r10p2.unit.ua>
2020-03-01 12:04:45 +0200

    1st merge with abondarenko

--- inc/ush.h
+++ inc/ush.h
@@ -16,10 +16,8 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <term.h>
-
 #include <stdio.h>
 #include <string.h>
-
 #include "libmx/inc/libmx.h"
 
 /* -------- */
@@ -72,51 +70,44 @@ typedef struct s_ush {
     t_variable *variable_list;
 }              t_ush;
 
-int mx_strarrlen(char **arr);
-void mx_check_commands(t_ush *ush, char **line, char **env);
-void mx_dealloc_termconf(t_termconf **termconf);
-void mx_dealloc_ush(t_ush **ush);
-void mx_export(t_export **export_list, char **command, char **env);
-void mx_pop_front_export(t_export **head);
-void mx_pop_front_variable(t_variable **head);
-void mx_process_creator(char **line);
-void mx_push_back_export(t_export **export, char **kv);
-void mx_read_environment(t_export **export_list, char **env);
-void mx_read_termconf(t_termconf *termconf);
-t_export *mx_exportnode_creation(void);
-// t_env *mx_envnode_creation(void);
-t_termconf *mx_create_termconf();
-t_ush *mx_create_ush();
-
-short mx_get_buf_type(char ch);
-
-int mx_buf_drop(char **buf, int *buf_size);
-int mx_buf_push(char **buf, int *buf_size, char ch);
-
-t_t_node *mx_create_text_node(char *text);
-void mx_pop_t_node_front(t_t_node **head);
-void mx_push_t_node_back(t_t_node **head, char *text);
-
-int mx_lstlen(t_b_node **head);
-t_b_node *mx_create_block_node(t_t_node *t_node);
-void mx_pop_block_front(t_b_node **head);
-t_b_node *mx_push_block_back(t_b_node **head, t_t_node *t_node);
-void mx_dealloc_blocks(t_b_node **head);
-
-// STRING OPERATIONS && PARSING
-void mx_parse_buf(t_ush *ush);
-t_b_node *mx_parse_block(t_t_node **head);
 char *mx_space_parse(char *str, int *piv);
 char *mx_text_parse(char *str, int *piv);
 char *mx_sinmrk_parse(char *str, int *piv);
 char *mx_doumrk_parse(char *str, int *piv);
-
-void mx_restore_buffer(t_ush *ush);
-int mx_term_width_check(t_ush *ush, int *len, int *term);
-
+int mx_buf_drop(char **buf, int *buf_size);
+int mx_buf_push(char **buf, int *buf_size, char ch);
 int mx_get_twidth();
-
+int mx_lstlen(t_b_node **head);
 int mx_read_input(t_ush *ush);
+int mx_strarrlen(char **arr);
+int mx_term_width_check(t_ush *ush, int *len, int *term);
+short mx_get_buf_type(char ch);
+// t_env *mx_envnode_creation(void);
+t_export *mx_exportnode_creation(void);
+t_b_node *mx_create_block_node(t_t_node *t_node);
+t_b_node *mx_parse_block(t_t_node **head);
+t_b_node *mx_push_block_back(t_b_node **head, t_t_node *t_node);
+t_termconf *mx_create_termconf();
+t_t_node *mx_create_text_node(char *text);
+t_ush *mx_create_ush();
+void mx_check_commands(t_ush *ush, char **line, char **env);
+void mx_dealloc_blocks(t_b_node **head);
+void mx_dealloc_termconf(t_termconf **termconf);
+void mx_dealloc_ush(t_ush **ush);
+void mx_export(t_export **export_list, char **command, char **env);
+void mx_parse_buf(t_ush *ush);
+void mx_pop_block_front(t_b_node **head);
+void mx_pop_front_export(t_export **head);
+void mx_pop_front_variable(t_variable **head);
+void mx_pop_t_node_front(t_t_node **head);
+void mx_process_creator(char **line);
+void mx_push_t_node_back(t_t_node **head, char *text);
+void mx_push_back_export(t_export **export, char **kv);
+void mx_read_environment(t_export **export_list, char **env);
+void mx_read_termconf(t_termconf *termconf);
+void mx_restore_buffer(t_ush *ush);
+
+// STRING OPERATIONS && PARSING
 
 /* -------- */
 
